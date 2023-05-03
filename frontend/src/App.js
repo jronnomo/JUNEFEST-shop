ff7d668cc8c9eb1e8fd376f990341ed6165a83cf
jronnomo <ggronnii@gmail.com>
2023-05-03 11:50:28 -0400

    wrapping up front end. product page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { Container } from "react-bootstrap";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to JUNEFEST</h1>
+          <Outlet />
         </Container>
       </main>
       <Footer />
