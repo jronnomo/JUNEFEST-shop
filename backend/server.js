4bb58bbfdaf43bb4ba4018075dc899fba4914be2
Jerry Ronnau <ggronnii@MacBook-Pro-12.local>
2023-05-05 13:33:30 -0400

    initial mongodb configuration setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import products from './data/products.js';
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+connectDB();
 app.get('/api/products', (req, res) => {
   res.json(products);
 });
