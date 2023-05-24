47a758e8df286e94503c93628e986b8df0f471a0
jronnomo <ggronnii@gmail.com>
2023-05-24 14:36:48 -0400

    added productRoutes file that connects to Mongo

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,18 @@
-import products from './data/products.js';
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 connectDB();
-app.get('/api/products', (req, res) => {
-  res.json(products);
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
 });
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
-
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => console.log(`Server running in ${process.env.NODE_ENV} on port ${port}`));
