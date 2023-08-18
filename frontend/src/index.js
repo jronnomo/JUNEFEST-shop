0e35aea4e232b9728cea6382a785d1b32cf7562c
jronnomo <ggronnii@gmail.com>
2023-08-17 22:25:59 -0400

    created Cart screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,41 +1,43 @@
 //Core React
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 //Router packages
-import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 
 //View Components
-import App from "./App";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
+import App from './App';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 //Redux Components
-import { Provider } from "react-redux";
-import store from "./store";
+import { Provider } from 'react-redux';
+import store from './store';
 
 //CSS
 //import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/bootstrap.custom.css";
-import "./assets/index.css";
+import './assets/bootstrap.custom.css';
+import './assets/index.css';
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<App />}>
-            <Route index={true} path="/" element={<HomeScreen />} />
-            <Route path="/product/:id" element={<ProductScreen />} />
-        </Route>
-    )
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
+      <Route path='/cart' element={<CartScreen />} />
+    </Route>
+  )
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <RouterProvider router={router} />
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
 );
 
 reportWebVitals();
