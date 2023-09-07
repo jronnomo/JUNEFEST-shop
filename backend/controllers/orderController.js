394e30886f2210e4c492894e974aa5f47968ee93
jronnomo <ggronnii@gmail.com>
2023-09-07 13:15:43 -0400

    filled Admin OrderList screen with table containing all orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -86,7 +86,8 @@ const updateOrderToDelivered = asyncHandler(async (req, res) => {
 //@route GET /api/orders
 //@access Private/Admin
 const getOrders = asyncHandler(async (req, res) => {
-  res.send('get all orders');
+  const orders = await Order.find({}).populate('user', 'name email');
+  res.status(200).json(orders);
 });
 
 export { addOrderItems, getMyOrders, getOrderById, updateOrderToDelivered, updateOrderToPaid, getOrders };
--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -35,6 +35,12 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
       }),
       keepUnusedDataFor: 5,
     }),
+    getOrders: builder.query({
+      query: () => ({
+        url: ORDERS_URL,
+      }),
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 export const {
@@ -43,4 +49,5 @@ export const {
   usePayOrderMutation,
   useGetPayPalClientIdQuery,
   useGetMyOrdersQuery,
+  useGetOrdersQuery,
 } = ordersApiSlice;
